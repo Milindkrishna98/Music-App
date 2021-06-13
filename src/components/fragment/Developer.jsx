add8f35e409c687eb9919261ac2e9d6a05c50497
Milindkrishna <40000951+Milindkrishna98@users.noreply.github.com>
2021-06-13 16:42:37 +0530

    react-js-music-app | developed by Milind Krishna
    music-app-reactjs

---
+++ src/components/fragment/Developer.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import '../assets/scss/Developer.scss';
+import {IconButton} from "@material-ui/core";
+import AvatarImage from "../assets/img/avatar.jpg";
+import {Facebook, Instagram, LinkedIn, Portrait, Twitter} from "@material-ui/icons";
+
+const Developer = () => {
+    return (
+        <div className={"Developer"}>
+            <h3 className={"Developer-head"}>Meet the Developer</h3>
+            <div className="Developer-profile">
+                <div className="Developer-profileCard">
+                    <img src={AvatarImage} alt="Profile"/>
+                    <div className={"Card-details"}>
+                        <h3>Milind Krishna</h3>
+                        <p>Full Stack developer</p>
+                        <p>Competitive Coder</p>
+                    </div>
+                </div>
+                <div className="Developer-profileDetails">
+                    <p>A Electronics And Communication and Engineering Student at SKFGI, Kolkata.</p>
+                    <p>Graduating in 2021 and looking for a responsible position to gain practical knowledge</p>
+                    <p>A full-stack web developer and a Competitive coder.</p>
+                    <p>I love designing fully responsive websites.</p>
+                    <p>I have a keen interest in developing projects, whenever I want to learn something new.</p>
+                    <p>And a Cricket Lover.</p>
+                    <div className="Card-btn">
+                        <IconButton target={"_blank"}  href={"https://www.facebook.com/"} title={"vishal.kld"}>
+                            <Facebook/>
+                        </IconButton>
+                        <IconButton target={"_blank"} href={"https://twitter.com/"}  title={"Vishal_kld"}>
+                            <Twitter/>
+                        </IconButton>
+                        <IconButton target={"_blank"} href={"https://www.linkedin.com/in/milindkrishna/"}  title={"vishalsingh-"}>
+                            <LinkedIn/>
+                        </IconButton>
+                        <IconButton target={"_blank"} href={"https://www.instagram.com/milind_kriss/"}  title={"vishalsingh023"}>
+                            <Instagram/>
+                        </IconButton>
+                        <IconButton target={"_blank"} href={"https://milind-resume.herokuapp.com/"}  title={"Web Portfolio"}>
+                            <Portrait/>
+                        </IconButton>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Developer;
